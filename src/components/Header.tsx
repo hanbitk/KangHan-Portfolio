d141940f93bfe8abd2ee310759c067d160b35c33
Hanbit Kang <hanbitkang95@gmail.com>
2023-10-10 21:18:41 +0900

    feat: header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
 import Navbar from './Navbar';
+import darkIcon from '../assets/header/dark.svg';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+  const navigate = useNavigate();
   return (
-    <div>
-      <Navbar />
-    </div>
+    <header className="w-full h-14 border-b-2 border-orientalBlue flex justify-between items-center px-5 py-4">
+      <div className="w-1/3 flex justify-between items-center">
+        {/* Logo */}
+        <h2
+          className="font-impact text-4xl text-orientalBlue cursor-pointer"
+          onClick={() => navigate('/')}
+        >
+          hanbit
+        </h2>
+        {/* Navbar */}
+        <Navbar />
+      </div>
+      <div>
+        {/* Dark/Light Mode Toggle */}
+        <img
+          src={darkIcon}
+          alt="Oriental Blue Moon Icon"
+          className="cursor-pointer"
+        />
+      </div>
+    </header>
   );
 };
 
---
+++ src/utils/constants.ts
@@ -0,0 +1,22 @@
+export const PATH_URL = {
+  ABOUT: '/about',
+  PROJECTS: '/projects',
+  CONTACT: '/contact',
+};
+
+export const TABS = [
+  {
+    title: 'ABOUT',
+    path: '/about',
+  },
+
+  {
+    title: 'PROJECTS',
+    path: '/projects',
+  },
+
+  {
+    title: 'CONTACT',
+    path: '/contact',
+  },
+];

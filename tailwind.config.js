92890ac242c62cf77093850204cb6d442c7ca246
Hanbit Kang <hanbitkang95@gmail.com>
2023-10-10 16:18:43 +0900

    tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,ts,tsx}'],
+  theme: {
+    colors: {
+      orientalBlue: '#364B82',
+      lightGray: '#E3DCDC',
+      lightBlack: '#373737',
+    },
+    fontFamily: {
+      impact: ['impact'],
+      helveticaNeue: ['Helvetica Neue'],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
